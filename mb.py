7c00cbbe51d7b1e1cb7069c68001fa0586dfb51d
James Pearson <james_pearson@outlook.com>
2019-11-04 21:01:42 +0000

    Added easily editable screen arrays

--- mb.py
+++ mb.py
@@ -2,6 +2,7 @@
 
 import time
 from bluezero import microbit
+
 ubit = microbit.Microbit(adapter_addr='B8:27:EB:0B:AA:BE',
                          device_addr='E6:51:A6:1A:37:5B',
                          accelerometer_service=True,
@@ -10,24 +11,48 @@ ubit = microbit.Microbit(adapter_addr='B8:27:EB:0B:AA:BE',
                          magnetometer_service=False,
                          pin_service=False,
                          temperature_service=True)
+
 looping = True
+
+ledX = [
+    0b10001,
+    0b01010,
+    0b00100,
+    0b01010,
+    0b10001]
+
+ledHappyFace = [
+    0b01010,
+    0b01010,
+    0b00100,
+    0b10001,
+    0b11111]
+
+ledSadFace = [
+    0b01010,
+    0b01010,
+    0b00100,
+    0b11111,
+    0b10001]
+
+
+
 ubit.connect()
 print('Connected... Press a button to select mode')
 mode = 0
 while looping:
     if ubit.button_a > 0 and ubit.button_b > 0:
         mode = 3
-        ubit.pixels = [0b10001, 0b01010, 0b00100, 0b01010, 0b10001]
+        ubit.pixels = ledX
         time.sleep(1)
     elif ubit.button_b > 0:
         mode = 2
-        ubit.pixels = [0b11111, 0b00100, 0b00100, 0b00100, 0b00100]
-
-        time.sleep(0.25)
+        ubit.pixels = ledSadFace
+        time.sleep(1)
     elif ubit.button_a > 0:
         mode = 1
-        ubit.pixels = [0b11110, 0b10000, 0b11100, 0b10000, 0b10000]
-        time.sleep(0.25)
+        ubit.pixels = ledHappyFace
+        time.sleep(1)
 
     if mode == 1:
         x, y, z = ubit.accelerometer
