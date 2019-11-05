60c176ce17821e7171b97df512ad4640344bdb40
James Pearson <james_pearson@outlook.com>
2019-11-05 22:02:24 +0000

    Update autoBT.sh

--- autoBT.sh
+++ autoBT.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+sudo hciconfig hci0 down
+sudo hciconfig hci0 up
+
 coproc bluetoothctl
-echo -e 'connect E6:51:A6:1A:37:5B\nexit' >&${COPROC[1]}
-output=$(cat <&${COPROC[0]})
-echo $output
+echo -e 'remove E6:51:A6:1A:37:5B\n'
+echo -e 'connect E6:51:A6:1A:37:5B\n'
+echo -e 'pair E6:51:A6:1A:37:5B\nexit'# >&${COPROC[1]}
+echo -e 'exit'
+#output=$(cat <&${COPROC[0]})
+#echo $output
