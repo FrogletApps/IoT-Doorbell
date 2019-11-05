49ec9563d393cf1b996a21d30fffc94bd406ae9a
James Pearson <james_pearson@outlook.com>
2019-11-05 21:29:37 +0000

    Add sense hat display output

--- mb.py
+++ mb.py
@@ -2,6 +2,10 @@
 
 import time
 from bluezero import microbit
+from sense_hat import SenseHat
+from picamera import PiCamera
+
+sense = SenseHat()
 
 ubit = microbit.Microbit(adapter_addr='B8:27:EB:0B:AA:BE',
                          device_addr='E6:51:A6:1A:37:5B',
@@ -14,7 +18,8 @@ ubit = microbit.Microbit(adapter_addr='B8:27:EB:0B:AA:BE',
 
 looping = True
 
-ledX = [
+#Microbit display arrays
+mbX = [
     0b10001,
     0b01010,
     0b00100,
@@ -22,7 +27,7 @@ ledX = [
     0b10001]
 
 #Currently used as Bluetooth connected icon
-ledHappyFace = [
+mbHappyFace = [
     0b00000,
     0b01010,
     0b00000,
@@ -30,51 +35,82 @@ ledHappyFace = [
     0b01110]
 
 #Currently used as Bluetooth disconnected icon
-ledSadFace = [
+mbSadFace = [
     0b00000,
     0b01010,
     0b00000,
     0b01110,
     0b10001]
 
-ledBellLeft = [
+mbBellLeft = [
     0b00100,
     0b01100,
     0b11100,
     0b01100,
     0b10100]
 
-ledBellCentre = [
+mbBellCentre = [
     0b00100,
     0b01110,
     0b01110,
     0b11111,
     0b00100]
 
-ledBellRight = [
+mbBellRight = [
     0b00100,
     0b01110,
     0b00111,
     0b00110,
     0b00101]
 
+#Sense HAT display arrays
+B = [0,0,255]  #Blue - use this for Bluetooth related stuff
+N = [0,0,0]    #Off
 
+#Currently used as Bluetooth connected icon
+piHappyFace = [
+N,N,N,N,N,N,N,N,
+N,B,B,N,N,B,B,N,
+N,B,B,N,N,B,B,N,
+N,N,N,N,N,N,N,N,
+N,N,N,N,N,N,N,N,
+N,B,N,N,N,N,B,N,
+N,N,B,B,B,B,N,N,
+N,N,N,N,N,N,N,N
+]
 
+#Currently used as Bluetooth disconnected icon
+piSadFace = [
+N,N,N,N,N,N,N,N,
+N,B,B,N,N,B,B,N,
+N,B,B,N,N,B,B,N,
+N,N,N,N,N,N,N,N,
+N,N,N,N,N,N,N,N,
+N,N,B,B,B,B,N,N,
+N,B,N,N,N,N,B,N,
+N,N,N,N,N,N,N,N
+]
+
+sense.set_pixels(piSadFace)
 ubit.connect()
+sense.set_pixels(piHappyFace)
 print('Connected... Press a button to select mode')
+
 mode = 0
 while looping:
     if ubit.button_a > 0 and ubit.button_b > 0:
         mode = 3
-        ubit.pixels = ledX
+        ubit.pixels = mbX
         time.sleep(1)
     elif ubit.button_b > 0:
         mode = 2
-        ubit.pixels = ledSadFace
+        ubit.pixels = mbSadFace
+        sense.set_pixels(piSadFace)
         time.sleep(1)
     elif ubit.button_a > 0:
         mode = 1
-        ubit.pixels = ledHappyFace
+        ubit.pixels = mbHappyFace
+        sense.set_pixels(piHappyFace)
         time.sleep(1)
 
     if mode == 1:
@@ -92,3 +128,4 @@ while looping:
         print('Exiting')
 
 ubit.disconnect()
+sense.clear()
