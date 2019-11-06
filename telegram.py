7c8b226c4f1f23e13614d075c372cb4e8bb0eb39
James Pearson <james_pearson@outlook.com>
2019-11-06 16:17:44 +0000

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -6,3 +6,5 @@ bot_message = "Bleep bloop I'm a bot c:"
 send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
 
 response = requests.get(send_text)
+
+print response.json()
