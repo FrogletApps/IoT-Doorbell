0b700a834d68e1f7f0c9dd7d6e4b8a6db825a6e6
James Pearson <james_pearson@outlook.com>
2019-11-06 20:09:47 +0000

    Picture experiment

--- pi/telegram.py
+++ pi/telegram.py
@@ -1,12 +1,29 @@
 import secret
+import time
 import os
 import requests
+from picamera import PiCamera
 
 #bot_message = "Bleep bloop I'm a bot c:"
 #send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
 #response = requests.get(send_text)
 
-send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID() + 'attach:///../../../image.jpg'
-response = requests.get(send_picture)
+picturePath = '/home/pi/image.jpg'
+
+camera = PiCamera()
+camera.start_preview()
+time.sleep(1)
+camera.capture(picturePath)
+camera.stop_preview()
+picture = open(picturePath, 'rb')
+camera.close()
+
+#Wait for 2 seconds
+time.sleep(2)
+#Close the photo
+camera.close()
+
+send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID()# + 'attach://' + picture
+response = requests.get(send_picture, picture)
 
 print (response.json())
