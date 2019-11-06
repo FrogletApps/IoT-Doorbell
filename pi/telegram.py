3eae3776cb09a4c661e13ab3f22260f8618b7c66
James Pearson <james_pearson@outlook.com>
2019-11-06 20:35:18 +0000

    Another attempt

--- pi/telegram.py
+++ pi/telegram.py
@@ -3,13 +3,10 @@ import time
 import os
 import requests
 from picamera import PiCamera
-import telegram
 
 #bot_message = "Bleep bloop I'm a bot c:"
 #send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
 #response = requests.get(send_text)
-
-bot = telegram.Bot(secret.tgBotKey)
 
 picturePath = '/home/pi/image.jpg'
 
@@ -18,7 +15,8 @@ camera.start_preview()
 time.sleep(1)
 camera.capture(picturePath)
 camera.stop_preview()
-picture = open(picturePath, 'rb')
+#picture = open(picturePath, 'rb')
+picture = {'file':open(picturePath, 'rb')}
 camera.close()
 
 #Wait for 2 seconds
@@ -26,9 +24,7 @@ time.sleep(2)
 #Close the photo
 camera.close()
 
-#send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID()# + 'attach://' + picture
-#response = requests.get(send_picture, picture)
-
-bot.send_photo(chat_id=secret.tgChatID(), photo=open(picturePath, 'rb'))
+send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID()# + 'attach://' + picture
+response = requests.get(send_picture, picture=picture)
 
 print (response.json())
