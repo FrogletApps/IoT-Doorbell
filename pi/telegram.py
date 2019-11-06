1543cc2f4f8d2adef0af7b37372d8971bc2a9b8c
James Pearson <james_pearson@outlook.com>
2019-11-06 19:42:22 +0000

    Test picture uploading

--- pi/telegram.py
+++ pi/telegram.py
@@ -2,9 +2,11 @@ import secret
 import os
 import requests
 
-bot_message = "Bleep bloop I'm a bot c:"
-send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
+#bot_message = "Bleep bloop I'm a bot c:"
+#send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
+#response = requests.get(send_text)
 
-response = requests.get(send_text)
+send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID() + '/home/pi/image.jpg'
+response = requests.get(send_picture)
 
 print (response.json())
