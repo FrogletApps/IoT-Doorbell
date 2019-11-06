fdcdf5d1d1cb0c0d048c99e8718b5ff1fc15074c
James Pearson <james_pearson@outlook.com>
2019-11-06 00:26:47 +0000

    Base PWA content

---
+++ PWA/iotDoorbell.js
@@ -0,0 +1,8 @@
+//Ensures that the page can work offline
+UpUp.start({
+    "content-url": "DateGenerator.html",
+    "assets":[
+        "DateGenerator.js",
+        "DateGenerator.css"
+    ]
+});
