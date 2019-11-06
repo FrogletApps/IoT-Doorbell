400eab7836a3afcd7c6f87574823ca96a3b47cb0
James Pearson <james_pearson@outlook.com>
2019-11-06 20:24:38 +0000

    Experiment with telegram bot library

--- pi/telegram.py
+++ pi/telegram.py
@@ -3,10 +3,13 @@ import time
 import os
 import requests
 from picamera import PiCamera
+import telegram
 
 #bot_message = "Bleep bloop I'm a bot c:"
 #send_text = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendMessage?chat_id=' + secret.tgChatID() + '&parse_mode=Markdown&text=' + bot_message
 #response = requests.get(send_text)
+
+bot = telegram.Bot(secret.tgBotKey)
 
 picturePath = '/home/pi/image.jpg'
 
@@ -23,7 +26,9 @@ time.sleep(2)
 #Close the photo
 camera.close()
 
-send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID()# + 'attach://' + picture
-response = requests.get(send_picture, picture)
+#send_picture = 'https://api.telegram.org/bot' + secret.tgBotKey() + '/sendPhoto?chat_id=' + secret.tgChatID()# + 'attach://' + picture
+#response = requests.get(send_picture, picture)
+
+bot.send_photo(chat_id=secret.tgChatID(), photo=open(picturePath, 'rb'))
 
 print (response.json())
