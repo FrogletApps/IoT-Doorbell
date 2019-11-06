40a25f348323c83dc7517a996228bec1dfbadee1
James Pearson <james_pearson@outlook.com>
2019-11-06 16:15:10 +0000

    Added telegram notifications

--- pushbullet.py
+++ pushbullet.py
@@ -1,4 +1,4 @@
 import secret
 import os
 
-os.system('curl -u' + secret.apiKey + ': https://api.pushbullet.com/v2/pushes -d type=link -d title="There\'s someone at the door" -d url="https://www.google.com"')
+os.system('curl -u' + secret.apiKey() + ': https://api.pushbullet.com/v2/pushes -d type=link -d title="There\'s someone at the door" -d url="https://www.google.com"')
---
+++ telegram.py
@@ -0,0 +1,8 @@
+import secret
+import os
+import requests
+
+bot_message = "Bleep bloop I'm a bot c:"
+send_text = 'https://api.telegram.org/bot' + secret.tgBotKey + '/sendMessage?chat_id=' + secret.tgChatID + '&parse_mode=Markdown&text=' + bot_message
+
+response = requests.get(send_text)
