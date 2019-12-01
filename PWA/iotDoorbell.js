364b617ce73ce13e02c5edef8a8f1721fb801505
James Pearson <james_pearson@outlook.com>
2019-12-01 22:07:41 +0000

    Reset web control stuff

--- PWA/iotDoorbell.js
+++
@@ -1,8 +0,0 @@
-//Ensures that the page can work offline
-UpUp.start({
-    "content-url": "DateGenerator.html",
-    "assets":[
-        "DateGenerator.js",
-        "DateGenerator.css"
-    ]
-});
