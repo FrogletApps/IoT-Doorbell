3c63fc911d960f1db99066d434ae8996409056a9
James Pearson <james_pearson@outlook.com>
2019-12-01 20:40:30 +0000

    Clean up unused files

--- pi/pushbullet.py
+++
@@ -1,4 +0,0 @@
-import secret
-import os
-
-os.system('curl -u' + secret.apiKey() + ': https://api.pushbullet.com/v2/pushes -d type=link -d title="There\'s someone at the door" -d url="https://www.google.com"')
