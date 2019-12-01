e2083f4a0dd853125b92be461f5930065a0db607
James Pearson <james_pearson@outlook.com>
2019-12-01 18:08:54 +0000

    Updated no wifi icon

--- pi/pi.py
+++ pi/pi.py
@@ -113,14 +113,14 @@ N,N,N,N,N,N,N,N
 
 #Pi no internet icon
 piNoWifi = [
-N,N,N,N,N,N,N,N,
-N,N,W,W,W,W,N,N,
-N,W,N,N,N,N,W,N,
-N,N,N,W,W,N,N,N,
-N,N,W,N,N,W,N,N,
-N,N,N,N,N,N,N,N,
-N,N,N,W,W,N,N,N,
-N,N,N,N,N,N,N,N
+R,N,N,N,N,N,N,N,
+N,R,W,W,W,W,N,N,
+N,W,R,N,N,N,W,N,
+N,N,N,R,W,N,N,N,
+N,N,W,N,R,W,N,N,
+N,N,N,N,N,R,N,N,
+N,N,N,W,W,N,R,N,
+N,N,N,N,N,N,N,R
 ]
 
 sense.set_pixels(piNoWifi)
