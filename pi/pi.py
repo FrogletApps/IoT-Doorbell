1ce6f52a84efd6e7bdf9b3b08ba375f8da6fb8d7
James Pearson <james_pearson@outlook.com>
2019-12-01 18:10:08 +0000

    No wifi icon 3

--- pi/pi.py
+++ pi/pi.py
@@ -116,9 +116,9 @@ piNoWifi = [
 R,N,N,N,N,N,N,N,
 N,R,W,W,W,W,N,N,
 N,W,R,N,N,N,W,N,
-N,N,N,R,W,N,N,N,
+W,N,N,R,W,N,N,W,
 N,N,W,N,R,W,N,N,
-N,N,N,N,N,R,N,N,
+N,W,N,N,N,R,W,N,
 N,N,N,W,W,N,R,N,
 N,N,N,N,N,N,N,R
 ]
