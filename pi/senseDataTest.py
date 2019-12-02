e4afceee671af33710a129066fc22a248d073fa2
James Pearson <james_pearson@outlook.com>
2019-12-02 11:46:10 +0000

    Create senseDataTest.py
    Testing code for sense hat sensors

---
+++ pi/senseDataTest.py
@@ -0,0 +1,25 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat
+
+def get_sense_data():
+    orientation = sense.get_orientation()
+    print("Accelerometer")
+    print(orientation["yaw"])
+    print(orientation["pitch"])
+    print(orientation["roll"])
+    gyro = sense.get_gyroscope_raw()
+    print("Gyro")
+    print(gyro["x"])
+    print(gyro["y"])
+    print(gyro["z"])
+    acc = sense.get_accelerometer_raw()
+    print("Accel raw")
+    print(acc["x"])
+    print(acc["y"])
+    print(acc["z"])
+    print(datetime.now())
+
+while True:
+    get_sense_data()
