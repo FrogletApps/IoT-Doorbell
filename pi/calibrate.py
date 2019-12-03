3a1989537d0045d76abe7129f237087e1fcbea9f
James Pearson <james_pearson@outlook.com>
2019-12-03 21:41:51 +0000

    Round earlier and reorganise code

--- pi/calibrate.py
+++ pi/calibrate.py
@@ -16,24 +16,23 @@ def averageData(allData):
     yawArray = []
     pitchArray = []
     rollArray = []
+    #Iterate through the data and sort it into different arrays (also round to nearest number)
     for group in allData:
         print(group)
-        yawArray.append(group[0])
-        pitchArray.append(group[1])
-        rollArray.append(group[2])
+        yawArray.append(round(group[0], 0))
+        pitchArray.append(round(group[1], 0))
+        rollArray.append(round(group[2], 0))
 
+    #Calculate range
     yawRange = max(yawArray) - min(yawArray)
     pitchRange = max(pitchArray) - min(pitchArray)
     rollRange = max(rollArray) - min(rollArray)
 
-    yawTotal = sum(yawArray)
-    pitchTotal = sum(pitchArray)
-    rollTotal = sum(rollArray)
-
+    #Calculate average
     arrayLength = len(allData)
-    yawAvg = round(yawTotal/arrayLength, 0)
-    pitchAvg = round(pitchTotal/arrayLength, 0)
-    rollAvg = round(rollTotal/arrayLength, 0)
+    yawAvg = sum(yawArray)/arrayLength
+    pitchAvg = sum(pitchArray)/arrayLength
+    rollAvg = sum(rollArray)/arrayLength
 
     return [[yawAvg, yawRange], [pitchAvg, pitchRange], [rollAvg, rollRange]]
 
