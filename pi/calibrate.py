846c89e134fba63a63c007fa23e1d594b3e17cf3
James Pearson <james_pearson@outlook.com>
2019-12-03 21:18:01 +0000

    Added calibrate.py

---
+++ pi/calibrate.py
@@ -0,0 +1,29 @@
+from csv import writer
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+#Collect 10 data samples of door position/orientation
+allData = []
+for x in range(0, 10):
+    allData.append(calibrate())
+    time.sleep(1)
+
+averageData(allData)
+
+def calibrate():
+    data = []
+    orientation = sense.get_orientation()
+    data.append(orientation["yaw"])
+    data.append(orientation["pitch"])
+    data.append(orientation["roll"])
+    
+def averageData(allData):
+    print(allData)
+
+def writeData():
+    with open ('calibate.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(data)
+
